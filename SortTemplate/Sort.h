06560b3f91969ea1b367ddbb2e215eb6edac093a
ForFishes <2282912238@qq.com>
2016-11-15 16:57:51 +0800

    选择排序

--- SortTemplate/Sort.h
+++ SortTemplate/Sort.h
@@ -1,14 +1,64 @@
 #pragma once
-template<typename Compare>
+#include<vector>
+#include<iostream>
+#include<functional>
+using namespace std;
 class CSort
 {
-
 public:
 	CSort();
 	~CSort();
+	template<typename Compare>
+	static bool isSorted(vector<Compare>& a)
+	{
+		for (size_t i = 1; i < a.size(); i++)
+		{
+			if (less(a[i], a[i-1]))return false;
+		}
+		return true;
+	}
+	//选择排序的实现，升序排列
+	template<typename Compare>
+	static void Selectsort(vector<Compare>& a)
+	{
+		int N = a.size();
+		for (int i = 0; i != N; ++i)
+		{
+			int min = i;
+			//首先寻找最小的角标
+			for (int j = i + 1; j != N; ++j)
+			{
+				if (less(a[j], a[min]))
+				{
+					min = j;
+				}
+			}
+			exch(a, i, min);
+		}
+	}
+	//插入排序
+	template<typename Compare>
+	static void show(vector<Compare>& a)
+	{
+		for (auto i : a)
+		{
+			cout << i << endl;
+		}
+	}
 private:
-	static bool less(Compare v, Compare w);
-	static void exch(Compare *a, int i, int j);
-	static void show(Compare *a);
+	template<typename Compare>
+	static bool less(Compare& v, Compare& w)
+	{
+		return v<w;//定义了小于
+				   //return std::less() < v, w > ;
+	}
+	template<typename Compare>
+	static void exch(vector<Compare>& a, int i, int j)
+	{
+		Compare t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+
 };
 
--- SortTemplate/SortTemplate.cpp
+++ SortTemplate/SortTemplate.cpp
@@ -2,9 +2,14 @@
 //
 
 #include "stdafx.h"
-
+#include"Sort.h"
+#include<assert.h>
 int main()
 {
+	vector<int>vec = { 7,3,2,5,7,8 };
+	CSort::Selectsort(vec);
+	assert(CSort::isSorted(vec));
+	CSort::show(vec);
     return 0;
 }
 
